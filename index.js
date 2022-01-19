64260e9b89044e2f2ef72d100692c7e9422e1093
Harsh-bitan <96148041+Harsh-bitan@users.noreply.github.com>
2022-01-19 12:42:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+   
+   var w =document.querySelector("#bitan");
+
+        
+ function fun1(){
+   w.innerHTML= "hello click here";
+  
+ }
+function hi(){
+    w.innerHTML="i am bitan chakraborty";
+}
